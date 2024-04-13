f16942b8529cadaf0a2a8cc25518adc84213ea5d
Terry Holmes <tchaser3@outlook.com>
2024-04-13 10:18:11 -0400

    Add project files.

---
+++ ImportEmployeeUpdates/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿/* Title:           Import Employee Updates
+ * Date:            10-9-18
+ * Author:          Terry Holmes
+ * 
+ * Description:     This will allow us to update employee information */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewEventLogDLL;
+using NewEmployeeDLL;
+using DepartmentDLL;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ImportEmployeeUpdates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //setting up the classes
+        WPFMessagesClass TheMessagesClass = new WPFMessagesClass();
+        EventLogClass TheEventLogClass = new EventLogClass();
+        EmployeeClass TheEmployeeClass = new EmployeeClass();
+        DepartmentClass TheDepartmentClass = new DepartmentClass();
+
+        FindSortedDepartmentDataSet TheFindSortedDepartmentDataSet = new FindSortedDepartmentDataSet();
+        FindActiveEmployeesDataSet TheFindActiveEmployeesDataSet = new FindActiveEmployeesDataSet();
+        EmployeeImportDataSet TheEmployeeImportDataSet = new EmployeeImportDataSet();
+        FindSortedEmployeeManagersDataSet TheFindSortedEmployeeManagersDataSet = new FindSortedEmployeeManagersDataSet();
+        public static UpdatesForEmployeeDataSet TheUpdatesForEmployeeDataSet = new UpdatesForEmployeeDataSet();
+        FindAllActiveEmployeeInformationDataSet TheFindAllActiveEmployeeInformationDataSet = new FindAllActiveEmployeeInformationDataSet();
+
+        public static int gintEmployeeID;
+        public static string gstrFirstName;
+        public static string gstrLastName;
+        public static string gstrDepartment;
+        public static string gstrSalaryType;
+        public static string gstrManagerName;
+        public static int gintManagerID;
+        public static bool gblnEmployeeFound;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void mitClose_Click(object sender, RoutedEventArgs e)
+        {
+            TheMessagesClass.CloseTheProgram();
+        }
+
+        private void dgrResults_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void mitImportExcel_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application xlDropOrder;
+            Excel.Workbook xlDropBook;
+            Excel.Worksheet xlDropSheet;
+            Excel.Range range;
+
+            int intColumnRange = 0;
+            int intCounter;
+            int intNumberOfRecords;
+            string strEmployeeID;
+            int intEmployeeID;
+            string strFirstName;
+            string strLastName;
+            string strDepartment;
+            string strPayID;
+            int intPayID;
+            string strSalaryType;
+            string strManagerID;
+            int intManagerID;
+            string strManagerFirstName;
+            string strManagerLastName;
+
+            try
+            {
+                TheEmployeeImportDataSet.employeeupdate.Rows.Clear();
+
+                Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+                dlg.FileName = "Document"; // Default file name
+                dlg.DefaultExt = ".xlsx"; // Default file extension
+                dlg.Filter = "Excel (.xlsx)|*.xlsx"; // Filter files by extension
+
+                // Show open file dialog box
+                Nullable<bool> result = dlg.ShowDialog();
+
+                // Process open file dialog box results
+                if (result == true)
+                {
+                    // Open document
+                    string filename = dlg.FileName;
+                }
+
+                PleaseWait PleaseWait = new PleaseWait();
+                PleaseWait.Show();
+
+                xlDropOrder = new Excel.Application();
+                xlDropBook = xlDropOrder.Workbooks.Open(dlg.FileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                xlDropSheet = (Excel.Worksheet)xlDropOrder.Worksheets.get_Item(1);
+
+                range = xlDropSheet.UsedRange;
+                intNumberOfRecords = range.Rows.Count;
+                intColumnRange = range.Columns.Count;
+
+                for (intCounter = 2; intCounter <= intNumberOfRecords; intCounter++)
+                {
+
+                    strEmployeeID = Convert.ToString((range.Cells[intCounter, 1] as Excel.Range).Value2);
+                    intEmployeeID = Convert.ToInt32(strEmployeeID);
+                    strFirstName = Convert.ToString((range.Cells[intCounter, 2] as Excel.Range).Value2);
+                    strLastName = Convert.ToString((range.Cells[intCounter, 3] as Excel.Range).Value2);
+                    strDepartment = Convert.ToString((range.Cells[intCounter, 4] as Excel.Range).Value2);
+                    strPayID = Convert.ToString((range.Cells[intCounter, 5] as Excel.Range).Value2);
+                    intPayID = Convert.ToInt32(strPayID);
+                    strSalaryType = Convert.ToString((range.Cells[intCounter, 6] as Excel.Range).Value2);
+                    strManagerID = Convert.ToString((range.Cells[intCounter, 7] as Excel.Range).Value2);
+                    intManagerID = Convert.ToInt32(strManagerID);
+                    strManagerFirstName = Convert.ToString((range.Cells[intCounter, 8] as Excel.Range).Value2);
+                    strManagerLastName = Convert.ToString((range.Cells[intCounter, 9] as Excel.Range).Value2);
+                    
+
+                    EmployeeImportDataSet.employeeupdateRow NewEmployeeRow = TheEmployeeImportDataSet.employeeupdate.NewemployeeupdateRow();
+
+                    NewEmployeeRow.Department = strDepartment;
+                    NewEmployeeRow.EmployeeID = intEmployeeID;
+                    NewEmployeeRow.FirstName = strFirstName;
+                    NewEmployeeRow.LastName = strLastName;
+                    NewEmployeeRow.ManagerFirstName = strManagerFirstName;
+                    NewEmployeeRow.ManagerID = intManagerID;
+                    NewEmployeeRow.ManagerLastName = strManagerLastName;
+                    NewEmployeeRow.PayID = intPayID;
+                    NewEmployeeRow.SalaryType = strSalaryType;
+
+                    TheEmployeeImportDataSet.employeeupdate.Rows.Add(NewEmployeeRow);
+                }
+
+                PleaseWait.Close();
+                dgrResults.ItemsSource = TheEmployeeImportDataSet.employeeupdate;
+                mitProcess.IsEnabled = true;
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Import Employee Update // Import Excel Menu Item " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TheFindSortedEmployeeManagersDataSet = TheEmployeeClass.FindSortedEmployeeManagers();
+
+            TheFindActiveEmployeesDataSet = TheEmployeeClass.FindActiveEmployees();
+        }
+
+        private void mitProcess_Click(object sender, RoutedEventArgs e)
+        {
+            //setting local veriables
+            int intCounter;
+            int intNumberOfRecords;
+            int intEmployeeID;
+            int intManagerID;
+            int intPayID;
+            string strDepartment;
+            string strSalaryType;
+            bool blnFatalError = false;
+
+            try
+            {
+                intNumberOfRecords = TheEmployeeImportDataSet.employeeupdate.Rows.Count - 1;
+
+                for(intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                {
+                    intEmployeeID = TheEmployeeImportDataSet.employeeupdate[intCounter].EmployeeID;
+                    intManagerID = TheEmployeeImportDataSet.employeeupdate[intCounter].ManagerID;
+                    intPayID = TheEmployeeImportDataSet.employeeupdate[intCounter].PayID;
+                    strDepartment = TheEmployeeImportDataSet.employeeupdate[intCounter].Department;
+                    strSalaryType = TheEmployeeImportDataSet.employeeupdate[intCounter].SalaryType;
+
+                    blnFatalError = TheEmployeeClass.UpdateEmployeePayInformation(intEmployeeID, strDepartment, strSalaryType, intManagerID, intPayID);
+
+                    if (blnFatalError == true)
+                        throw new Exception();
+                }
+
+                TheFindAllActiveEmployeeInformationDataSet = TheEmployeeClass.FindAllActiveEmployeeInformation();
+
+                dgrResults.ItemsSource = TheFindAllActiveEmployeeInformationDataSet.FindAllActiveEmployeeInformation;
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Import Employee Updates // Main Window // Process Menu Item " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+            
+        }
+    }
+}
---
+++ ImportEmployeeUpdates/UpdateEmployees.xaml.cs
@@ -0,0 +1,319 @@
+﻿/* Title:           Update Employee
+ * Date:            10-9-18
+ * Author:          Terry Holmes
+ * 
+ * Description:     This is used to update the employee */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using NewEmployeeDLL;
+using NewEventLogDLL;
+using DepartmentDLL;
+
+namespace ImportEmployeeUpdates
+{
+    /// <summary>
+    /// Interaction logic for UpdateEmployees.xaml
+    /// </summary>
+    public partial class UpdateEmployees : Window
+    {
+        WPFMessagesClass TheMessagesClass = new WPFMessagesClass();
+        EmployeeClass TheEmployeeClass = new EmployeeClass();
+        EventLogClass TheEventLogClass = new EventLogClass();
+        DepartmentClass TheDepartmentClass = new DepartmentClass();
+
+        //setting up the data
+        FindSortedDepartmentDataSet TheFindSortedDepartmentDataSet = new FindSortedDepartmentDataSet();
+        FindAllEmployeesByLastNameDataSet TheFindAllEmployeesByLastNameDataSet = new FindAllEmployeesByLastNameDataSet();
+        FindSortedEmployeeManagersDataSet TheFindSortedEmployeeManagersDataSet = new FindSortedEmployeeManagersDataSet();
+        FindWarehousesDataSet TheFindWarehousesDataSet = new FindWarehousesDataSet();
+        FindEmployeeByEmployeeIDDataSet TheFindEmployeeByEmployeeIDDataSet = new FindEmployeeByEmployeeIDDataSet();
+
+        public UpdateEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void cboSelectEmployee_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int intSelectedIndex;
+            int intCounter;
+            int intNumberOfRecords;
+            bool blnActive;
+            string strEmployeeType;
+            string strHomeOffice;
+            string strGroup;
+            int intComboSelectedIndex = 0;
+            string strSalaryType;
+            string strDepartment;
+            int intManagerID;
+
+
+            try
+            {
+                intSelectedIndex = cboSelectEmployee.SelectedIndex - 1;
+
+                if (intSelectedIndex > -1)
+                {
+                    MainWindow.gintEmployeeID = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].EmployeeID;
+
+                    TheFindEmployeeByEmployeeIDDataSet = TheEmployeeClass.FindEmployeeByEmployeeID(MainWindow.gintEmployeeID);
+
+                    txtEmployeeID.Text = Convert.ToString(MainWindow.gintEmployeeID);
+                    txtFirstName.Text = TheFindEmployeeByEmployeeIDDataSet.FindEmployeeByEmployeeID[0].FirstName;
+                    txtLastName.Text = TheFindEmployeeByEmployeeIDDataSet.FindEmployeeByEmployeeID[0].LastName;
+                    txtPhoneNumber.Text = TheFindEmployeeByEmployeeIDDataSet.FindEmployeeByEmployeeID[0].PhoneNumber;
+
+                    blnActive = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].Active;
+                    strEmployeeType = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].EmployeeType;
+                    strHomeOffice = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].HomeOffice;
+                    strGroup = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].EmployeeGroup;
+
+                    if (TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].IsEmailAddressNull() == false)
+                    {
+                        txtEmailAddress.Text = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].EmailAddress;
+                    }
+                    else
+                    {
+                        txtEmailAddress.Text = "";
+                    }
+                    if (TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].IsDepartmentNull() == true)
+                    {
+                        strDepartment = "";
+
+                        cboSelectDepartment.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        strDepartment = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].Department;
+
+                        for (intCounter = 0; intCounter <= cboSelectDepartment.Items.Count - 1; intCounter++)
+                        {
+                            if (strDepartment == cboSelectDepartment.SelectedItem.ToString())
+                            {
+                                cboSelectDepartment.SelectedIndex = intCounter;
+                            }
+                        }
+                    }
+
+                    if (TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].IsManagerIDNull() == true)
+                    {
+                        intManagerID = 0;
+
+                        cboSelectManager.SelectedIndex = 0;
+                    }
+                    else if (TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].ManagerID == 0)
+                    {
+                        intManagerID = 0;
+
+                        cboSelectManager.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        intManagerID = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].ManagerID;
+
+                        intNumberOfRecords = TheFindSortedEmployeeManagersDataSet.FindSortedEmployeeManagers.Rows.Count - 1;
+
+                        for (intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                        {
+                            if (intManagerID == TheFindSortedEmployeeManagersDataSet.FindSortedEmployeeManagers[intCounter].employeeID)
+                            {
+                                cboSelectManager.SelectedIndex = intCounter + 1;
+                            }
+                        }
+                    }
+
+                    if (TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].IsSalaryTypeNull() == true)
+                    {
+                        strSalaryType = "";
+
+                        cboSelectSalaryType.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        strSalaryType = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].SalaryType;
+
+                        for (intCounter = 0; intCounter < cboSelectSalaryType.Items.Count; intCounter++)
+                        {
+                            cboSelectSalaryType.SelectedIndex = intCounter;
+
+                            if (strSalaryType == cboSelectSalaryType.SelectedItem.ToString())
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].IsDepartmentNull() == true)
+                    {
+                        cboSelectDepartment.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        strDepartment = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intSelectedIndex].Department;
+
+                        for (intCounter = 0; intCounter < cboSelectDepartment.Items.Count; intCounter++)
+                        {
+                            cboSelectDepartment.SelectedIndex = intCounter;
+
+                            if (strDepartment == cboSelectDepartment.SelectedItem.ToString())
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (blnActive == true)
+                        cboSelectActive.SelectedIndex = 1;
+                    else
+                        cboSelectActive.SelectedIndex = 2;
+
+                    intNumberOfRecords = cboSelectGroup.Items.Count - 1;
+
+                    for (intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                    {
+                        cboSelectGroup.SelectedIndex = intCounter;
+
+                        if (cboSelectGroup.SelectedItem.ToString() == strGroup)
+                        {
+                            intComboSelectedIndex = intCounter;
+                        }
+                    }
+
+                    cboSelectGroup.SelectedIndex = intComboSelectedIndex;
+
+                    intNumberOfRecords = cboSelectHomeOffice.Items.Count - 1;
+
+                    for (intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                    {
+                        cboSelectHomeOffice.SelectedIndex = intCounter;
+
+                        if (cboSelectHomeOffice.SelectedItem.ToString() == strHomeOffice)
+                        {
+                            intComboSelectedIndex = intCounter;
+                        }
+                    }
+
+                    cboSelectHomeOffice.SelectedIndex = intComboSelectedIndex;
+
+                    intNumberOfRecords = cboSelectEmployeetype.Items.Count - 1;
+
+                    for (intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                    {
+                        cboSelectEmployeetype.SelectedIndex = intCounter;
+
+                        if (cboSelectEmployeetype.SelectedItem.ToString() == strEmployeeType)
+                        {
+                            intComboSelectedIndex = intCounter;
+                        }
+                    }
+
+                    cboSelectEmployeetype.SelectedIndex = intComboSelectedIndex;
+                }
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Import Employee Updates // Update Employee // Select Employee Combo Box " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+        }
+
+        private void cboSelectActive_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboSelectGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboSelectHomeOffice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboSelectEmployeetype_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboSelectSalaryType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboSelectDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboSelectManager_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this will call the like search
+            int intNumberOfRecords;
+            int intCounter;
+            int intLength;
+
+            try
+            {
+                intLength = MainWindow.gstrLastName.Length;
+
+                if (intLength > 2)
+                {
+                    cboSelectEmployee.Items.Clear();
+                    cboSelectEmployee.Items.Add("Select Employee");
+
+                    TheFindAllEmployeesByLastNameDataSet = TheEmployeeClass.FindAllEmployeesByLastName(MainWindow.gstrLastName);
+
+                    intNumberOfRecords = TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName.Rows.Count - 1;
+
+                    if (intNumberOfRecords == -1)
+                    {
+                        TheMessagesClass.ErrorMessage("Employees Not Found");
+                        return;
+                    }
+
+                    for (intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                    {
+                        cboSelectEmployee.Items.Add(TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intCounter].FirstName + " " + TheFindAllEmployeesByLastNameDataSet.FindAllEmployeeByLastName[intCounter].LastName);
+                    }
+
+                    cboSelectEmployee.SelectedIndex = 0;
+                }
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Importedd Employee Update // Edit Employee // Enter Last Name Event " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+        }
+
+        private void mitClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
